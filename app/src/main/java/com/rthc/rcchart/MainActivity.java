e7be309b46eed2c65ce3b47614ed349b9a76c174
wdjxysc <wdjxysc@163.com>
2016-05-31 17:02:57 +0800

    commit

--- app/src/main/java/com/rthc/rcchart/MainActivity.java
+++ app/src/main/java/com/rthc/rcchart/MainActivity.java
@@ -3,21 +3,51 @@ package com.rthc.rcchart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    RcPieChart rcPieChart;
+    RcPieChart rcPieChart1;
+    RcPieChart rcPieChart2;
+    RcPieChart rcPieChart3;
+
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rcPieChart = (RcPieChart) findViewById(R.id.pieChart);
+        rcPieChart1 = (RcPieChart) findViewById(R.id.pieChart1);
+        rcPieChart2 = (RcPieChart) findViewById(R.id.pieChart2);
+        rcPieChart3 = (RcPieChart) findViewById(R.id.pieChart3);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        if( button!= null)
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(rcPieChart != null)
-                    rcPieChart.setProgress((float) Math.random());
+                if (rcPieChart1 != null) {
+                    float progress1 = (float) Math.random();
+                    rcPieChart1.setProgress(progress1);
+                    textView1.setText((int) (progress1 * 100) + "%");
+                }
+                if (rcPieChart2 != null) {
+                    float progress2 = (float) Math.random();
+                    rcPieChart2.setProgress(progress2);
+                    textView2.setText((int) (progress2 * 100) + "%");
+                }
+                if (rcPieChart3 != null) {
+                    float progress3 = (float) Math.random();
+                    rcPieChart3.setProgress(progress3);
+                    textView3.setText((int) (progress3 * 100) + "%");
+                }
             }
         });
     }
--- app/src/main/java/com/rthc/rcchart/RcPieChart.java
+++ app/src/main/java/com/rthc/rcchart/RcPieChart.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -28,12 +29,18 @@ public class RcPieChart extends View {
         super(context, attrs, defStyleAttr);
     }
 
-    int mPieColor;
+    int mPieColor = Color.CYAN;
+    int mCircleSecondColor = Color.LTGRAY;
+    int mPieBackColor = Color.TRANSPARENT;
     float mPieProgress;
-    float mLineWidth;
+    int mCircleWidth = 0;//默认为0 就是扇形
 
     Paint mPaintBack;
     Paint mPaintTop;
+
+    int radius;
+
+
 
     void setupAttrs(AttributeSet attrs){
 
@@ -46,17 +53,28 @@ public class RcPieChart extends View {
             switch (attr) {
                 case R.styleable.RcPieChart_pieColor:
                     // 默认颜色设置为灰色
-                    mPieColor = a.getColor(attr, Color.GRAY);
+                    mPieColor = a.getColor(attr, Color.LTGRAY);
+                    break;
+                case R.styleable.RcPieChart_circleSecondColor:
+                    // 默认颜色设置为灰色
+                    mCircleSecondColor = a.getColor(attr, Color.LTGRAY);
+                    break;
+                case R.styleable.RcPieChart_pieBackColor:
+                    // 默认颜色设置为透明
+                    mPieBackColor = a.getColor(attr, Color.TRANSPARENT);
                     break;
                 case R.styleable.RcPieChart_pieProgress:
                     // 默认设置为16sp，TypeValue也可以把sp转化为px
                     mPieProgress = a.getFloat(attr, 0.6f);
                     break;
+                case R.styleable.RcPieChart_circleWidth:
+                    mCircleWidth = (int)a.getDimension(attr,radius);
             }
-
         }
 
         a.recycle();
+
+
 
         //初始化画笔
         mPaintBack = new Paint();
@@ -79,26 +97,29 @@ public class RcPieChart extends View {
 //                getHeight()/2,
 //                getWidth()/3, mPaint);
 
-
-        int radius;
         if (getWidth() > getHeight()) {
             radius = getHeight() / 2;
         } else {
             radius = getWidth() / 2;
         }
+
+
         RectF rect = new RectF(getWidth() / 2 - radius, getHeight() / 2 - radius, getWidth() / 2 + radius, getHeight() / 2 + radius);
 
-        mPaintBack.setColor(Color.LTGRAY);
+        mPaintBack.setColor(mPieBackColor);
         canvas.drawArc(rect, -90, 360, false, mPaintBack);
 
-        mLineWidth = radius;
-        RectF rectCircle = new RectF(getWidth() / 2 - radius + mLineWidth/2,
-                getHeight() / 2 - radius+ mLineWidth/2,
-                getWidth() / 2 + radius - mLineWidth/2,
-                getHeight() / 2 + radius- mLineWidth/2);
-        mPaintTop.setColor(mPieColor);
+        if(mCircleWidth == 0) mCircleWidth = radius;
+        RectF rectCircle = new RectF(getWidth() / 2 - radius + mCircleWidth /2,
+                getHeight() / 2 - radius+ mCircleWidth /2,
+                getWidth() / 2 + radius - mCircleWidth /2,
+                getHeight() / 2 + radius- mCircleWidth /2);
+        mPaintTop.setColor(mCircleSecondColor);
         mPaintTop.setStyle(Paint.Style.STROKE);
-        mPaintTop.setStrokeWidth(mLineWidth);
+        mPaintTop.setStrokeWidth(mCircleWidth);
+        canvas.drawArc(rectCircle, -90, 360, false, mPaintTop);
+
+        mPaintTop.setColor(mPieColor);
         canvas.drawArc(rectCircle, -90, 360 * mPieProgress, false, mPaintTop);
 
         Log.i("log", "getWidth()--" + getWidth() + "     getHeight()--" + getHeight());
@@ -116,7 +137,6 @@ public class RcPieChart extends View {
                 int frameIntervalTime = 10;//每帧间隔时间
 
                 float interval = progress - mPieProgress;
-//        if(interval<0) interval = -interval;
 
                 for (int i = 0;i < animatorTime; i+=frameIntervalTime){
                     mPieProgress += interval/(animatorTime/frameIntervalTime);
@@ -140,5 +160,13 @@ public class RcPieChart extends View {
      */
     public float getProgress() {
         return mPieProgress;
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        event.getAction();
+        return super.onTouchEvent(event);
     }
 }
